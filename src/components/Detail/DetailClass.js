f108dc0189a2101505a936db80d480abe0e7d907
axelschein <axelschein@gmail.com>
2022-05-09 14:20:23 -0300

    Class component logic finish

--- src/components/Detail/DetailClass.js
+++ src/components/Detail/DetailClass.js
@@ -4,11 +4,63 @@ import '../../App.css';
 class DetailClass extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      img: '',
+      desImg: 'Back image',
+    };
+  }
+
+  handleOnClick() {
+    this.state.desImg === 'Back image'
+      ? this.setState({ desImg: 'Front image' })
+      : this.setState({ desImg: 'Back image' });
+  }
+
+  handleImageChange() {
+    console.log('handle');
+    this.state.img === this.props.pokeDescription.sprites?.front_default
+      ? this.setState({
+          img: this.props.pokeDescription.sprites?.back_default,
+        })
+      : this.setState({
+          img: this.props.pokeDescription.sprites?.front_default,
+        });
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ img: this.props.pokeDescription.sprites?.front_default });
+      console.log('mount');
+    }, 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.pokeDescription !== this.props.pokeDescription) {
+      this.setState({
+        img: this.props.pokeDescription.sprites?.front_default,
+      });
+      console.log('update2');
+    }
+    if (prevState.desImg !== this.state.desImg) {
+      this.handleImageChange();
+      console.log('update1');
+    }
   }
 
   render() {
     const { pokeDescription } = this.props;
+
+    const pokeType =
+      pokeDescription.types !== undefined
+        ? pokeDescription.types[0].type.name
+        : 'loading';
+
+    const pokeAbilities =
+      pokeDescription.abilities !== undefined
+        ? pokeDescription.abilities[0].ability.name +
+          ' and ' +
+          pokeDescription.abilities[1].ability.name
+        : 'loading';
 
     return pokeDescription === {} ? (
       <p>loading...</p>
@@ -16,6 +68,22 @@ class DetailClass extends React.Component {
       <div>
         <h2>Hey I'm Class </h2>
         <h3>{pokeDescription.name}</h3>
+        <img
+          src={this.state.img}
+          loading="lazy"
+          alt={pokeDescription.name}
+          style={{ width: '100px' }}
+        />
+
+        <button onClick={() => this.setState(this.handleOnClick())}>
+          {this.state.desImg}
+        </button>
+
+        <p>Type: {pokeType}</p>
+        <p>Weight: {pokeDescription.weight} lbs</p>
+        <p>Height: {pokeDescription.weight} "</p>
+        <p>Number: {pokeDescription.order}</p>
+        <p>Abilities: {pokeAbilities}</p>
       </div>
     );
   }
