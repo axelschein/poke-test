c108de66e28daa1e8aaefe37f5cc686be59cf26e
axelschein <axelschein@gmail.com>
2022-05-09 12:28:03 -0300

    Bringing poke data ton functional

--- src/components/Detail/DetailFunctional.js
+++ src/components/Detail/DetailFunctional.js
@@ -1,12 +1,29 @@
-import './Detail.css';
+import '../../App.css';
 
 function DetailFunctional({ pokeDescription }) {
+  const pokeType =
+    pokeDescription.types !== undefined
+      ? pokeDescription.types[0].type.name
+      : 'loading';
+  const pokeAbilities =
+    pokeDescription.abilities !== undefined
+      ? pokeDescription.abilities[0].ability.name +
+        ' and ' +
+        pokeDescription.abilities[1].ability.name
+      : 'loading';
   return pokeDescription === {} ? (
     <p>loading...</p>
   ) : (
-    <div>
+    <div className="container">
       <h2>Hey I'm functional </h2>
       <h3>{pokeDescription.name}</h3>
+      <div className="container"></div>
+
+      <p>Type: {pokeType}</p>
+      <p>Weight: {pokeDescription.weight} lbs</p>
+      <p>Height: {pokeDescription.weight}"</p>
+      <p>Number: {pokeDescription.order}</p>
+      <p>Abilities: {pokeAbilities}</p>
     </div>
   );
 }
